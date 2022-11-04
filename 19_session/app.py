18f7bf78ae65daf1b02322b012b32b74613afdcf
49PES <vsaboo30@stuy.edu>
2022-11-04 08:43:54 -0400

    Updating K19

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,52 +1,57 @@
 # Brian, Vansh, Weichen
 # SoftDev
 # K19 -- Sessions Greetings
-# 2022-11-3
+# 2022-11-03 
 # time spent: 30 mins
 
-from flask import Flask             #facilitate flask webserving
+
+from flask import Flask, redirect, url_for             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-from flask import session           
-from flask import redirect
-from flask import url_for 
+from flask import session
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/')
+
+users = {'m':'donky'}
+
+
+@app.route("/") #, methods=['GET', 'POST'])
 def index():
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    # return 'You are not logged in'
-    return redirect(url_for('/login'))
+        return render_template('response.html',
+        user=session["username"])
+    return render_template( 'login.html',)
 
 
+@app.route("/auth") # , methods=[ 'POST'])
+def authenticate():
+    if request.method == 'GET':
+        user = request.args.get('username')
+        password = request.args.get('password')
+        if user not in users.keys(): #check if user exists
+            error = "User DNE"
+            return render_template('login.html',
+            error=error)
+        if password != users[user]: #check if password matches user
+            error = "Wrong Password"
+            return render_template('login.html',
+            error=error)
+        session["username"] = user
+        return redirect(url_for('index'))  #redirects to home page
 
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
-
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    # remove the username from the session if it's there
     session.pop('username', None)
     return redirect(url_for('index'))
 
-    
+
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
