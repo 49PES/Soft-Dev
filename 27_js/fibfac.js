1531eb82ab64e7fcbecbaf0dd4a62f2ecd3086dd
Vansh <vsaboo30@stuy.edu>
2023-04-05 07:02:52 -0400

    updating files

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,8 +1,7 @@
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
+// Team GuacNoSauce :: Vansh Saboo, Samantha Hua
+// SoftDev pd7
 // K27 -- Basic functions in JavaScript
-// 2023-04-04t
-// --------------------------------------------------
+// 2023-04-03t
 
 
 // as a duo...
@@ -11,3 +10,21 @@
 
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function Factorial(n) {
+    if (n < 2) {
+        return 1;
+    }
+    else {
+        return n * Factorial( n - 1 );
+    }
+}
+
+function Fibonacci(n) {
+    if (n < 2) {
+        return 1;
+    }
+    else {
+        return n * Factorial( n - 1 );
+    }
+}
